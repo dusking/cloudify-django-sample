074ee6315c6557aa077d362a5a4f3cb62788678f
Omer Duskin <omerd@gigaspaces.com>
2016-07-10 13:41:31 +0300

    use ctx download_resource_and_render

--- scripts/nginx/preconfigure_nginx.py
+++ scripts/nginx/preconfigure_nginx.py
@@ -1,29 +1,18 @@
 #!/usr/bin/env python
 
 import os
-import socket
-import time
-import subprocess
-import sys
-from os.path import join, dirname
 from cloudify import ctx
 
 
-# deploy blueprint utils resource
-ctx.download_resource(resource_path='sampleutils.py', target_path=join(dirname(__file__), 'sampleutils.py'))
-sys.path.append(dirname(__file__))
-import sampleutils
-
-
 def _set_nginx_config_file(host_ip):
-    config_file = os.path.join(os.path.expanduser('~'), 'djangosample/src/mysite/nginx.conf')
-    project_root_dir = os.path.join(os.path.expanduser('~'), 'djangosample/src')
+    project_root_dir = os.path.expanduser('~/djangosample/src')
+    config_file = os.path.join(project_root_dir, 'mysite/nginx.conf')
     params = ctx.source.node.properties.copy()
     params.update({'host_ip': host_ip,
                    'project_root_dir': project_root_dir})
-    sampleutils.download_resource_and_render(source='config/nginx/nginx.conf.template',
-                                             destination=config_file,
-                                             params=params)
+    ctx.download_resource_and_render(resource_path='config/nginx/nginx.conf.template',
+                                     target_path=config_file,
+                                     template_variables=params)
 
 
 def main():
